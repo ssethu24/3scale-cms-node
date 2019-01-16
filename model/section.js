e66062b21c9a8a26f66e5a4a927e938b858f3f10
safi <ssethu@encoress.com>
2019-01-16 20:49:13 +0530

    page completed

--- model/section.js
+++ model/section.js
@@ -6,6 +6,7 @@ module.exports = class Section extends Base {
     this.sections = Array.isArray(sections) ? sections : [];
     this.SN = {};
     this.PP = {};
+    this.TITLE_PATH = {};
     this.load(this.sections);
   }
 
@@ -16,10 +17,29 @@ module.exports = class Section extends Base {
     }
   }
 
+  getTitlePathUtil(item) {
+    let title = '';
+    if (_.isEmpty(item.parent_id)) {
+      title += ''
+    } else {
+      title += item.title + '/' + this.getTitlePathUtil(this.ID[item.parent_id]);
+    }
+    return title
+  }
+
+  getTitlePath(item) {
+    return this.getTitlePathUtil(item).split('/').reverse().join('/');
+  }
+
   triggerMap() {
-    this.SN = _.keyBy(this.sections, 'system_name');
     this.ID = _.keyBy(this.sections, 'id');
+    this.sections.forEach((section) => {
+      section.titlePath = this.getTitlePath(section);
+      this.ID[section.id].titlePath = section.titlePath ? section.titlePath : '/'
+    });
+    this.SN = _.keyBy(this.sections, 'system_name');
     this.PP = _.keyBy(this.sections, 'partial_path');
+    this.TITLE_PATH = _.keyBy(this.sections, 'titlePath');
   }
 
   getById(id) {
@@ -27,6 +47,7 @@ module.exports = class Section extends Base {
   }
 
   addOrUpdateSection(section) {
+    section.titlePath = this.getTitlePath(section);
     if (this.ID[section.id]) {
       const index = this.sections.findIndex(item => item.id === section.id);
       this.sections[index] = section;
@@ -36,6 +57,7 @@ module.exports = class Section extends Base {
     this.ID[section.id] = section;
     this.SN[section.system_name] = section;
     this.PP[section.partial_path] = section;
+    this.TITLE_PATH[section.titlePath] = section;
   }
 
   //Key should be id or sn
@@ -45,6 +67,7 @@ module.exports = class Section extends Base {
       delete this.ID[item.id];
       delete this.SN[item.system_name];
       delete this.PP[item.partial_path];
+      delete this.TITLE_PATH[item.titlePath];
       const index = this.sections.findIndex(item => item.id === section.id);
       delete this.sections[index];
     }
@@ -56,6 +79,10 @@ module.exports = class Section extends Base {
 
   getSectionByPartialPath(name) {
     return this.PP[name] ? this.PP[name] : null;
+  }
+
+  getSectionByTitlePath(name) {
+    return this.TITLE_PATH[name] ? this.TITLE_PATH[name] : null;
   }
 
   getSystemName(id) {
--- uploadv2.js
+++ uploadv2.js
@@ -7,21 +7,27 @@ const utils = require('./utils');
 const logger = require('./logger');
 const { Section, File, Layout, Partial, BuiltinPage } = require('./model');
 
-async function getSectionByPath(sectionPath) {
-  const section = Section.getSectionByPartialPath(sectionPath);
+async function getSectionByPath(sectionPath, isTitle = false) {
+  let section = null;
+  if (isTitle) {
+    section = Section.getSectionByPartialPath(sectionPath);
+  } else {
+    section = Section.getSectionByTitlePath(sectionPath);
+  }
   if (section) {
     return section;
   } else {
     // create new Section
     const sectionDirs = sectionPath.split('/');
     const title = sectionDirs.pop();
-    const parent = await getSectionByPath(path.join('/', sectionDirs.join('/')));
+    const parent = await getSectionByPath(path.join('/', sectionDirs.join('/')), isTitle);
+    const systemName = sectionPath.replace(/\s+/g, '_').toLowerCase();
     const section = {
-      partial_path: sectionPath,
+      partial_path: systemName,
       parent_id: parent.id,
       title: title,
       public: true,
-      system_name: sectionPath,
+      system_name: systemName,
     };
     const newSession = await api.createSection(section);
     Section.addOrUpdateSection(newSession);
@@ -112,10 +118,10 @@ async function uploadBuiltinPage(dir) {
   const metaFiles = utils.getMetaFileLocation(path.join(dir, '/builtin_page'));
   for (const metaFile of metaFiles) {
     const builtinPages = await fse.readJson(metaFile);
+    const dirPaths = metaFile.split('/');
+    dirPaths.pop();
     for (const builtinPage of builtinPages) {
       const oldBuiltinPage = Partial.getBySystemName(builtinPage.system_name);
-      const dirPaths = metaFile.split('/');
-      dirPaths.pop();
       builtinPage.filePath = path.join(dirPaths.join('/'), builtinPage.file_name);
       if (oldBuiltinPage) {
         logger.info("Update Partial", { systemName: builtinPage.system_name });
@@ -128,27 +134,46 @@ async function uploadBuiltinPage(dir) {
 }
 
 async function uploadPage(dir) {
+  const basePath = path.join(dir, '/pages');
+  const metaFiles = utils.getMetaFileLocation(basePath);
+  for (const metaFile of metaFiles) {
+    const pages = await fse.readJson(metaFile);
+    let titlePath = metaFile.replace(basePath, '').replace('/.meta.json', '');
+    titlePath = titlePath ? titlePath : '/';
 
+    console.log(titlePath);
+    console.log(Section.TITLE_PATH[titlePath]);
+    const section = await getSectionByPath(titlePath, true);
+    const dirPaths = metaFile.split('/');
+    dirPaths.pop();
+    for (const page of pages) {
+      //Find section and for this Path
+      page.filePath = path.join(dirPaths.join('/'), page.file_name);
+      page.section_id = section.id;
+      logger.info("Create Page", { sectionTitle: section.title, systemName: page.file_name });
+      await api.createPage(page);
+    }
+  }
 }
 
 async function upload(WRK_DIR) {
-  let templates = await api.list('template');
-  Layout.load(templates);
-  Partial.load(templates);
-  BuiltinPage.load(templates);
+  /*  let templates = await api.list('template');
+    Layout.load(templates);
+    Partial.load(templates);
+    BuiltinPage.load(templates);*/
   let destSections = await api.list('section');
   Section.load(destSections);
 
-  await uploadLayouts(WRK_DIR);
-  await uploadPartial(WRK_DIR);
+  /*  await uploadLayouts(WRK_DIR);
+    await uploadPartial(WRK_DIR);
 
-  await uploadBuiltinPage(WRK_DIR);
+    await uploadBuiltinPage(WRK_DIR);*/
   await uploadPage(WRK_DIR);
 
-  let destFiles = await api.list('file');
-  File.load(destFiles);
-  const baseDirPath = path.join(WRK_DIR, '/files');
-  await uploadFile(baseDirPath, baseDirPath);
+  /*  let destFiles = await api.list('file');
+    File.load(destFiles);
+    const baseDirPath = path.join(WRK_DIR, '/files');
+    await uploadFile(baseDirPath, baseDirPath);*/
 }
 
 module.exports = upload;
